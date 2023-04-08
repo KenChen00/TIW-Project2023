03767d8da3caf246f7710bc3d143614b0d7d57ac
KenChen <ken.chen@mail.polimi.it>
2023-04-08 22:21:16 +0200

    File Management

--- TIW_Project_100/src/main/java/it/polimi/ServerMain.java
+++ TIW_Project_100/src/main/java/it/polimi/ServerMain.java
@@ -5,6 +5,9 @@ public class ServerMain {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.print("ciao");
+		System.out.print("Hello");
+		//dadada
+		System.out.print(false);
 
 	}
 
