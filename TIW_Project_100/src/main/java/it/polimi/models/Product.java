a7088364c32d9e86a51a4b8eaa55c24550b1f3a4
Leo <74366736+Leomyh@users.noreply.github.com>
2023-04-09 21:58:19 +0200

    update product and sell class

--- TIW_Project_100/src/main/java/it/polimi/models/Product.java
+++ TIW_Project_100/src/main/java/it/polimi/models/Product.java
@@ -1,5 +1,50 @@
 package it.polimi.models;
 
 public class Product {
+	private String code;
+	private String name;
+	private String category;
+	private String photo;
+	private String description;
+	
+	public String getCode() {
+		return code;
+	}
+	
+	public void setCode(String code) {
+		this.code = code;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getCategory() {
+		return category;
+	}
+	
+	public void setCategory(String category) {
+		this.category = category;
+	}
+	
+	public String getPhoto() {
+		return photo;
+	}
+	
+	public void setPhoto(String photo) {
+		this.photo = photo;
+	}
+	
+	public String getDescription() {
+		return description;
+	}
+	
+	public void setDescription(String description) {
+		this.description = description;
+	}
 
 }
--- TIW_Project_100/src/main/java/it/polimi/models/Sell.java
+++ TIW_Project_100/src/main/java/it/polimi/models/Sell.java
@@ -1,5 +1,23 @@
 package it.polimi.models;
 
 public class Sell {
+	private String codeProduct;
+	private String codeSeller;
+	
+	public String getCodeProduct() {
+		return codeProduct;
+	}
+	
+	public void setCodeProduct(String codeProduct) {
+		this.codeProduct = codeProduct;
+	}
+	
+	public String getCodeSeller() {
+		return codeSeller;
+	}
+	
+	public void setCodeSeller(String codeSeller) {
+		this.codeSeller = codeSeller;
+	}
 
 }
